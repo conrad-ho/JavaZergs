f02537e43853c8462f09adb0b4e28d061db14b01
conrad-ho <conrad.ho@outlook.com>
2019-08-01 13:22:22 -0700

    broodlord, hellokity and corruptors

--- Zergs/Zergs.java
+++ Zergs/Zergs.java
@@ -1,24 +1,53 @@
 class Zergs {
     public static void main(String[] args) {
         System.out.println("Game started");
-        Larva l = new Larva();
-        Zerg z = l.spawn(Larva.ULTRALISK);
-        System.out.println(z);
+        Larva littlePest = new Larva();
+        Zerg hellokitty = littlePest.spawn(Larva.CORRUPTOR);
+        System.out.println(hellokitty);
+        Zerg brood = ((Corruptor)hellokitty).mutate();
+        System.out.println(brood);
+    }
+}
+
+class Terran {
+    private int legs;
+    // accessor
+    int getLegs() {
+        return legs;
+    } 
+    // setter
+    void setLegs(int newLegs) {
+        legs = newLegs;
+    }
+    public String toString() {
+        return "I am a " + this.getClass().getName() + " with " + legs + " legs!";
     }
 }
 
 class Zerg {
     private int claws;
+    // accessor
     int getClaws() {
         return claws;
     } 
+    // setter
     void setClaws(int newClaws) {
         claws = newClaws;
     }
     public String toString() {
         return "I am a " + this.getClass().getName() + " with " + claws + " claws!";
     }
+    Zerg(){
+        claws = 2;
+    }
 }
+
+
+
+
+
+
+
 
 class Hydralisk extends Zerg{
     Hydralisk() {
@@ -27,28 +56,78 @@ class Hydralisk extends Zerg{
 }
 
 class Ultralisk extends Zerg{
-    Hydralisk() {
+    int tusks;
+    Ultralisk() {
+        tusks = 2;
         setClaws(2);
     }
 }
 
+class Mutalisk extends Zerg {
+    int wings;
+    Mutalisk() {
+        wings = 2;
+        setClaws(0); 
+    }
+    public String toString(){
+        return super.toString() + " I can fly!";
+    }
+}
+
+class Corruptor extends Zerg {
+    int tentacles;
+    Mutalisk() {
+        tentacles = 8;
+        setClaws(0); 
+    }
+    public String toString(){
+        return super.toString() + " I can fly!";
+    }
+
+    Broodlord mutate(){
+        return new Broodlord();
+    }
+}
+
+class Broodlord extends Corruptor {
+    int wings;
+    Broodlord() {
+        wings = 2;
+        tentacles = 0;
+        setClaws(0); 
+    }
+    public String toString(){
+        return super.toString() + " GG to you!";
+    }
+}
+
 class Larva {
+    public final static int TERRAN = 34;
     public final static int HYDRALISK = 22;
     public final static int MUTALISK = 33;
     public final static int ULTRALISK = 99;
+    public final static int CORRUPTOR = 44;
 
     Zerg spawn(int type){
         Zerg newZerg = null;
         switch(type) {
-            case Larva.HYDRALISK:
-                newZerg = new Hydralisk();
-                break;
-            case Larva.MUTALISK:
-                newZerg = new Zerg();
-                break;
-            case Larva.ULTRALISK:
-                newZerg =  new Ultralisk();
-                break;
+        // case Larva.TERRAN:
+        //     newZerg = new Terran();
+        //     break;
+        case Larva.HYDRALISK:
+            newZerg = new Hydralisk();
+            break;
+        case Larva.MUTALISK:
+            newZerg = new Mutalisk();
+            break;
+        case Larva.ULTRALISK:
+            newZerg =  new Ultralisk();
+            break;
+        case Larva.CORRUPTOR:
+            newZerg =  new Corruptor();
+            break;
+        
+        
         }
         return newZerg;
     }
